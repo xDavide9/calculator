10103c16c02108afc98ed6d1313b011368918c40
Davide Bianco <biancodavide646@gmail.com>
2022-02-21 17:45:43 +0100

    add icon

--- src/main/java/com/xdavide9/calculator/CalculatorApplication.java
+++ src/main/java/com/xdavide9/calculator/CalculatorApplication.java
@@ -3,6 +3,7 @@ package com.xdavide9.calculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
@@ -15,8 +16,23 @@ public class CalculatorApplication extends Application {
         System.setProperty("prism.lcdtext", "false");
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculatorSimple.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        //stylesheet
+        try {
+            scene.getStylesheets().add(Objects.requireNonNull(CalculatorApplication.class.getResource("darcula.css")).toExternalForm());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.err.println("Could not set stylesheet");
+        }
+        //icon
+        try {
+            String path = Objects.requireNonNull(CalculatorApplication.class.getResource("icon.png")).toExternalForm();
+            stage.getIcons().add(new Image(path));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.err.println("Could not set icon");
+        }
+
         stage.setTitle("Calculator");
-        scene.getStylesheets().add(Objects.requireNonNull(CalculatorApplication.class.getResource("darcula.css")).toExternalForm());
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
